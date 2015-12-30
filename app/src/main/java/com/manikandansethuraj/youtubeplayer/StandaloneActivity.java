1800ae6785436b4ec2daf19c2f3040b9bbaf9b42
Manikandan Sethuraj <manikandansethu1987@gmail.com>
2015-12-30 01:21:15 -0500

    Added all the six playlists. made Standalone Player View as Home Page.
    Signed-off-by: Manikandan Sethuraj <manikandansethu1987@gmail.com>

--- app/src/main/java/com/manikandansethuraj/youtubeplayer/StandaloneActivity.java
+++ app/src/main/java/com/manikandansethuraj/youtubeplayer/StandaloneActivity.java
@@ -4,18 +4,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.youtube.player.YouTubeStandalonePlayer;
 
 public class StandaloneActivity extends AppCompatActivity
-implements View.OnClickListener{
+        implements View.OnClickListener{
 
     private String GOOGLE_API_KEY = "AIzaSyC_VvdBfMTB7Tg_gSAAWXeqVeHbKj7RWBY";
-    private String YOUTUBE_VIDEO_ID = "NjsY2k84uaw";
-    private String YOUTUBE_PLAYLIST = "PLMC9KNkIncKtPzgY-5rmhvj7fax8fdxoj";
-    private Button btnPlayVideo;
-    private Button btnPlayPlaylist;
+    private String YOUTUBE_PLAYLIST_MY_FAV = "LLIQBd4tvpYoSqrTdIluHAUA";
+    private String YOUTUBE_PLAYLIST_FOOTBALL = "PLMC9KNkIncKtPzgY-5rmhvj7fax8fdxoj";
+    private String YOUTUBE_PLAYLIST_CRICKET = "PLtineFwLo0jc_RT2VEtwSYP48796yAUep";
+    private String YOUTUBE_PLAYLIST_BEST_OF_2015 = "PLvFYFNbi-IBFeP5ALr50hoOmKiYRMvzUq";
+    private String YOUTUBE_PLAYLIST_TRAILERS = "PLScC8g4bqD47c-qHlsfhGH3j6Bg7jzFy-";
+    private String YOUTUBE_PLAYLIST_TECHNOLOGY = "PLNyYK0P0pNzvlE4Ph3SLrNHqhgLn4drTZ";
+    private ImageButton btnPlayMyFav;
+    private ImageButton btnPlayCricket;
+    private ImageButton btnPlayFootball;
+    private ImageButton btnPlayBestOf2015;
+    private ImageButton btnPlayTrailers;
+    private ImageButton btnPlayTechnology;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +33,19 @@ implements View.OnClickListener{
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
-        btnPlayVideo = (Button) findViewById(R.id.btnPlayVideo);
-        btnPlayPlaylist = (Button) findViewById(R.id.btnPlayPlaylist);
+        btnPlayMyFav = (ImageButton) findViewById(R.id.btnPlayMyFav);
+        btnPlayCricket = (ImageButton) findViewById(R.id.btnPlayCricket);
+        btnPlayBestOf2015 = (ImageButton) findViewById(R.id.btnPlayPlayBestOf2015);
+        btnPlayFootball = (ImageButton) findViewById(R.id.btnPlayFootball);
+        btnPlayTechnology = (ImageButton) findViewById(R.id.btnPlayTech);
+        btnPlayTrailers = (ImageButton) findViewById(R.id.btnPlayTrailers);
 
-        btnPlayVideo.setOnClickListener(this);
-        btnPlayPlaylist.setOnClickListener(this);
+        btnPlayCricket.setOnClickListener(this);
+        btnPlayMyFav.setOnClickListener(this);
+        btnPlayTrailers.setOnClickListener(this);
+        btnPlayBestOf2015.setOnClickListener(this);
+        btnPlayTechnology.setOnClickListener(this);
+        btnPlayFootball.setOnClickListener(this);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -44,11 +61,23 @@ implements View.OnClickListener{
     public void onClick(View v) {
         Intent intent = null;
         switch (v.getId()) {
-            case R.id.btnPlayVideo:
-                intent = YouTubeStandalonePlayer.createVideoIntent(this, GOOGLE_API_KEY, YOUTUBE_VIDEO_ID);
+            case R.id.btnPlayCricket:
+                intent = YouTubeStandalonePlayer.createPlaylistIntent(this, GOOGLE_API_KEY, YOUTUBE_PLAYLIST_CRICKET);
                 break;
-            case R.id.btnPlayPlaylist:
-                intent = YouTubeStandalonePlayer.createPlaylistIntent(this, GOOGLE_API_KEY, YOUTUBE_PLAYLIST);
+            case R.id.btnPlayMyFav:
+                intent = YouTubeStandalonePlayer.createPlaylistIntent(this, GOOGLE_API_KEY, YOUTUBE_PLAYLIST_MY_FAV);
+                break;
+            case  R.id.btnPlayFootball:
+                intent = YouTubeStandalonePlayer.createPlaylistIntent(this, GOOGLE_API_KEY, YOUTUBE_PLAYLIST_FOOTBALL);
+                break;
+            case  R.id.btnPlayPlayBestOf2015:
+                intent = YouTubeStandalonePlayer.createPlaylistIntent(this, GOOGLE_API_KEY, YOUTUBE_PLAYLIST_BEST_OF_2015);
+                break;
+            case  R.id.btnPlayTech:
+                intent = YouTubeStandalonePlayer.createPlaylistIntent(this, GOOGLE_API_KEY, YOUTUBE_PLAYLIST_TECHNOLOGY);
+                break;
+            case  R.id.btnPlayTrailers:
+                intent = YouTubeStandalonePlayer.createPlaylistIntent(this, GOOGLE_API_KEY, YOUTUBE_PLAYLIST_TRAILERS);
                 break;
             default:
 
